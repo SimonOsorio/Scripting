bacb5d8a6ffce8a03723e694c6c275b14410afb1
SimonOsorio <77751464+SimonOsorio@users.noreply.github.com>
2021-01-28 17:00:10 -0500

    Retos Tutoria 2

---
+++ Reto1Tutoria2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Reto1Tutoria2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de IMC\n");
+            Console.Write("Ingrese su peso (Kg): ");
+            double w = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese su altura (m): ");
+            double h = double.Parse(Console.ReadLine());
+
+            double IMC = w / (h * h);
+
+            Console.WriteLine("Su IMC es: " + IMC);
+
+
+            if (IMC < 18.5)
+            {
+                Console.WriteLine("Su peso es inferior al normal");
+            }
+            if (18.5 < IMC && IMC < 24.9)
+            {
+                Console.WriteLine("Su peso es normal");
+            }
+            if (24.9 < IMC && IMC <= 29.9)
+            {
+                Console.WriteLine("Su peso es superior al normal");
+            }
+            if (30 <= IMC)
+            {
+                Console.WriteLine("Usted sufre de obesidad");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Reto2Tutoria2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Reto2Tutoria2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de ecuación cuadrática (ax^2 + bx + c");
+            Console.WriteLine("Ingrese el componente a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el componente b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el componente c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double d = b * b - 4 * a * c;
+            double x, x1, x2;
+
+            if (d == 0)
+            {
+                x = -b / (2*a);
+                Console.WriteLine("El resultado es x = " + x);
+
+            }
+            if (d < 0)
+            {
+                Console.WriteLine("No es posible calcular una solución");
+            }
+            if (d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("El resultado es x1 = " + x1);
+                Console.WriteLine("El resultado es x2 = " + x2);
+            }
+
+        }
+    }
+}
