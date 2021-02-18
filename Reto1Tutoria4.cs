e26ccb04a8c1a83327848c1d6aa6292a11f65976
SimonOsorio <77751464+SimonOsorio@users.noreply.github.com>
2021-02-18 09:31:23 -0500

    Add files via upload

---
+++ Reto1Tutoria4.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Reto1Tutoria4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continua = true;
+            int cartaA = 0;
+            int cartaB = 0;
+            int nuevaCarta = 0;
+            int totalTemp = 0;            
+
+            Random randomGenerator = new Random();
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("           Blackjack");
+            Console.WriteLine("------------------------------------");
+
+            cartaA = randomGenerator.Next(1, 11);
+            cartaB = randomGenerator.Next(1, 11);
+            totalTemp = cartaA + cartaB;
+            Console.WriteLine("Sus cartas son:\n" + cartaA + " y " + cartaB);
+            Console.WriteLine("Lleva un total de: " + totalTemp);
+
+
+            while (continua == true)
+            {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("¿Desea más cartas? (S/N)");
+                string continuar = Console.ReadLine();
+                switch (continuar)
+                {
+                    case ("S"):
+                        continua = true;
+                        nuevaCarta = randomGenerator.Next(1, 10);
+                        totalTemp = totalTemp + nuevaCarta;
+                        Console.WriteLine("Su nueva carta es: " + nuevaCarta);
+                        Console.WriteLine("Lleva un total de: " + totalTemp);
+                        if (totalTemp == 21)
+                        {
+                            Console.WriteLine("------------------------------------");
+                            Console.WriteLine("     Usted ha ganado el juego");
+                            Console.WriteLine("------------------------------------");
+                            continua = false;
+                        }
+                        if (totalTemp > 21)
+                        {
+                            Console.WriteLine("------------------------------------");
+                            Console.WriteLine("     Usted ha perdido el juego");
+                            Console.WriteLine("------------------------------------");
+                            continua = false;
+                        }
+                        break;
+                    case ("s"):
+                        continua = true;
+                        nuevaCarta = randomGenerator.Next(1, 10);
+                        totalTemp = totalTemp + nuevaCarta;
+                        Console.WriteLine("Su nueva carta es: " + nuevaCarta);
+                        Console.WriteLine("Lleva un total de: " + totalTemp);
+                        if (totalTemp == 21)
+                        {
+                            Console.WriteLine("------------------------------------");
+                            Console.WriteLine("     Usted ha ganado el juego");
+                            Console.WriteLine("------------------------------------");
+                            continua = false;
+                        }
+                        if (totalTemp > 21)
+                        {
+                            Console.WriteLine("------------------------------------");
+                            Console.WriteLine("     Usted ha perdido el juego");
+                            Console.WriteLine("------------------------------------");
+                            continua = false;
+                        }
+                        break;
+                    default:
+                        continua = false;
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine("        Juego Finalizado");
+                        Console.WriteLine("------------------------------------");
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Reto1Tutoria5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Reto1Tutoria5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomGenerator = new Random();
+            int sumaCorrecta = 1;
+            int suma = 0;
+            int counter = 0;
+            Console.WriteLine("Captcha: Ingrese la suma de los 2 dígitos: ");
+
+            while (suma != sumaCorrecta)
+            {
+                if (counter < 3)
+                {
+                    int a = randomGenerator.Next(1, 11);
+                    int b = randomGenerator.Next(1, 11);
+                    Console.WriteLine(a + " + " + b);
+                    sumaCorrecta = a + b;
+                    suma = int.Parse(Console.ReadLine());
+                    counter += 1;
+                    if (suma != sumaCorrecta)
+                    {
+                        Console.WriteLine("Entrada errónea, vuelva a intentarlo");
+                        Console.WriteLine((3 - counter) + " intentos restantes");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No tiene más intentos");
+                    counter += 1;
+                    suma = sumaCorrecta;
+                }
+            }
+
+            if (counter <= 3)
+            {
+                Console.WriteLine("Entrada satisfactoria");
+            }
+        }
+    }
+}
