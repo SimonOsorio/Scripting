f5fdd87d4c6f7d59c0c26f0c200ce9e24e5bef12
SimonOsorio <77751464+SimonOsorio@users.noreply.github.com>
2021-02-28 15:27:03 -0500

    Reto 1 Tutoría 6

---
+++ Reto1Tutoria6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reto1Tutoria6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = { "Nezuko", "Tanjiro", "Zenitsu", "Inosuke", "Genya", "Kanao", "Tomioka" };
+            double[] estatura = { 153, 165, 164.5, 164, 180, 156, 176 };
+            double[] distancias = new double[estatura.Length];
+            int indice = 0;
+            double min = 200, total = 0;            
+            for (int i = 0; i < estatura.Length; i++) { total += estatura[i]; }
+            double promedioEstaturas = total / estatura.Length;
+            Console.WriteLine("Promedio de estaturas: " + promedioEstaturas);            
+            for (int i = 0; i < estatura.Length; i++)
+            {
+                distancias[i] = Math.Abs(promedioEstaturas - estatura[i]);
+                if (distancias[i] < min) { min = distancias[i]; indice = i;  }                
+            }
+            Console.WriteLine("Persona con estatura más cercana al promedio: " + nombres[indice] + " (" + estatura[indice] + ")"); ;
+        }
+    }
+}
